f91be51f161f8a75c41808be9fb5c1fef64368bc
ArielUndurraga <ariel.undurraga@usach.cl>
2017-08-31 14:34:17 -0700

    Test commit for films deployment

--- src/main/java/cl/citiaps/spring/backend/entities/Film.java
+++ src/main/java/cl/citiaps/spring/backend/entities/Film.java
@@ -6,7 +6,7 @@ import java.sql.Timestamp;
 
 
 /**
- * The persistent class for the actor database table.
+ * The persistent class for the film database table.
  * 
  */
 @Entity
@@ -19,32 +19,32 @@ public class Film implements Serializable {
 	@Column(name="film_id", unique=true, nullable=false)
 	private int filmId;
 
-	@Column(name="title", nullable=false, length=45)
+	@Column(name="title", nullable=false, length=100)
 	private String title;
 
-	@Column(name="description", nullable=false, length=45)
+	@Column(name="description", nullable=false)
 	private String decription;
 	
 	@Column(name="release_year", nullable=false)
 	private int releaseYear;
 
-	@Column(name="language_id", nullable=false)
-	private int languageId;
+	//@Column(name="language_id", nullable=false)
+	//private int languageId;
 	
-	@Column(name="original_language_id", nullable=true)
-	private int originalLanguageId;
+	//@Column(name="original_language_id", nullable=true)
+	//private int originalLanguageId;
 	
 	@Column(name="rental_duration", nullable=false)
 	private int rentalDuration;
 	
 	@Column(name="rental_rate", nullable=false)
-	private float rentalRate;
+	private double rentalRate;
 	
 	@Column(name="length", nullable=false)
 	private int length;
 	
 	@Column(name="replacement_cost", nullable=false)
-	private float replacementCost;
+	private double replacementCost;
 	
 	@Column(name="rating", nullable=false, length=20)
 	private String rating;
@@ -55,6 +55,108 @@ public class Film implements Serializable {
 	@Column(name="last_update", nullable=false)
 	private Timestamp lastUpdate;
 
+	public int getFilmId() {
+		return filmId;
+	}
 
+	public void setFilmId(int filmId) {
+		this.filmId = filmId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDecription() {
+		return decription;
+	}
+
+	public void setDecription(String decription) {
+		this.decription = decription;
+	}
+
+	public int getReleaseYear() {
+		return releaseYear;
+	}
+
+	public void setReleaseYear(int releaseYear) {
+		this.releaseYear = releaseYear;
+	}
+/*
+	public int getLanguageId() {
+		return languageId;
+	}
+
+	public void setLanguageId(int languageId) {
+		this.languageId = languageId;
+	}
+
+	public int getOriginalLanguageId() {
+		return originalLanguageId;
+	}
+
+	public void setOriginalLanguageId(int originalLanguageId) {
+		this.originalLanguageId = originalLanguageId;
+	}
+*/
+	public int getRentalDuration() {
+		return rentalDuration;
+	}
+
+	public void setRentalDuration(int rentalDuration) {
+		this.rentalDuration = rentalDuration;
+	}
+
+	public double getRentalRate() {
+		return rentalRate;
+	}
+
+	public void setRentalRate(double rentalRate) {
+		this.rentalRate = rentalRate;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public double getReplacementCost() {
+		return replacementCost;
+	}
+
+	public void setReplacementCost(double replacementCost) {
+		this.replacementCost = replacementCost;
+	}
+
+	public String getRating() {
+		return rating;
+	}
+
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+
+	public String getSpecialFeatures() {
+		return specialFeatures;
+	}
+
+	public void setSpecialFeatures(String specialFeatures) {
+		this.specialFeatures = specialFeatures;
+	}
+
+	public Timestamp getLastUpdate() {
+		return lastUpdate;
+	}
+
+	public void setLastUpdate(Timestamp lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
 
 }
